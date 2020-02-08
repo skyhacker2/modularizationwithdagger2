6039d293bba9883daa7e0c172aa97f24eabcb115
Eleven <skyhacker11@gmail.com>
2020-02-09 01:12:38 +0800

    更新代码

--- Readme.md
+++ Readme.md
@@ -126,7 +126,26 @@ public class App extends Application {
     }
 }
 ```
+`ComponentManager`的基本实现
+```
+public class ComponentManager {
+    private static Map<Class<?>, ComponentResolver> sComponentMap;
 
+    public static void init(Map<Class<?>, ComponentResolver> componentMap) {
+        sComponentMap = componentMap;
+    }
+
+    public static <T> T component(Class<? extends IComponent> clasz) {
+        try {
+            return (T) sComponentMap.get(clasz).getComponent();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
+```
 
 Demo的结构图：
 ![](images/demo_modules.png)
--- navigation/src/main/java/io/github/skyhacker2/navigation/navigator/ComponentManager.java
+++ navigation/src/main/java/io/github/skyhacker2/navigation/navigator/ComponentManager.java
@@ -6,15 +6,15 @@ import io.github.skyhacker2.export_api.ComponentResolver;
 import io.github.skyhacker2.export_api.IComponent;
 
 public class ComponentManager {
-    private static Map<Class<?>, ComponentResolver> sServicesMap;
+    private static Map<Class<?>, ComponentResolver> sComponentMap;
 
-    public static void init(Map<Class<?>, ComponentResolver> servicesMap) {
-        sServicesMap = servicesMap;
+    public static void init(Map<Class<?>, ComponentResolver> componentMap) {
+        sComponentMap = componentMap;
     }
 
     public static <T> T component(Class<? extends IComponent> clasz) {
         try {
-            return (T)sServicesMap.get(clasz).getComponent();
+            return (T) sComponentMap.get(clasz).getComponent();
         } catch (Exception e) {
             e.printStackTrace();
         }
